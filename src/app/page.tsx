27283225ebf7ae6ee97a29aec75d87d4877232cc
wmouton <wmouton.dev@gmail.com>
2024-04-15 10:19:24 +0200

    Update built by link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-24 w-full items-end justify-center bg-black lg:static lg:size-auto lg:bg-none">
           <a
             className="text-white pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://wmouton.vercel.app"
             target="_blank"
             rel="noopener noreferrer"
           >
